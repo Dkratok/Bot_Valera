3aa9620038187e2cf85c132b116f49910ebfbd46
Dkratok <Dkratok@gmail.com>
2020-12-27 18:58:33 +0300

    Cruze bot upd

--- cruze_bot.py
+++ cruze_bot.py
@@ -30,28 +30,28 @@ def get_text_messages(message):
             bot.send_message(message.from_user.id, rfl.read_whole_file('data/dealers.txt'), parse_mode='Markdown')
         elif ('инструкция' in req) | ('мануал' in req) | ('manual' in req):
             bot.send_document(message.from_user.id, open('media/cruze_manual.pdf', 'rb').read())
-        elif ('чат' in req) | ('chat' in req):
+        elif ('клуб' in req) | ('чат' in req) | ('chat' in req):
             bot.send_message(message.from_user.id, 'https://t.me/cruzefamily_minsk')
         elif 'дворник' in req:
             bot.send_message(message.from_user.id, rfl.read_whole_file('data/wipers.txt'), parse_mode='Markdown')
         elif ('колес' in req) | ('колёс' in req) | ('диски' in req):
             bot.send_message(message.from_user.id, rfl.read_whole_file('data/wheels.txt'), parse_mode='Markdown')
         elif ('рубль' or 'курс рубля') in req:
-            bot.send_message(message.from_user.id, 'Курс рос. рубля на сегодня ' + today_date_str + ' (за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub))));
-            bot.send_message(message.from_user.id, ' Курс рос. рубля на завтра ' + tomorrow_date_str + '(за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub, tomorrow_date_str))));
+            bot.send_message(message.from_user.id, 'Курс рос. рубля на сегодня ' + today_date_str + ' (за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub))))
+            bot.send_message(message.from_user.id, ' Курс рос. рубля на завтра ' + tomorrow_date_str + '(за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub, tomorrow_date_str))))
         elif ('доллар' or 'курс доллара') in req:
-            bot.send_message(message.from_user.id, 'Курс доллара на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol))));
-            bot.send_message(message.from_user.id, ' Курс доллара на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol, tomorrow_date_str))));
+            bot.send_message(message.from_user.id, 'Курс доллара на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol))))
+            bot.send_message(message.from_user.id, ' Курс доллара на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol, tomorrow_date_str))))
         elif ('евро' or 'курс евро') in req:
-            bot.send_message(message.from_user.id, 'Курс евро на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro))));
-            bot.send_message(message.from_user.id, ' Курс евро на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro, tomorrow_date_str))));
+            bot.send_message(message.from_user.id, 'Курс евро на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro))))
+            bot.send_message(message.from_user.id, ' Курс евро на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro, tomorrow_date_str))))
         elif ('курсы' or 'курсы валют') in req:
-            bot.send_message(message.from_user.id, 'Курс доллара на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol))));
-            bot.send_message(message.from_user.id, ' Курс доллара на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol, tomorrow_date_str))));
-            bot.send_message(message.from_user.id, 'Курс евро на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro))));
-            bot.send_message(message.from_user.id, ' Курс евро на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro, tomorrow_date_str))));
-            bot.send_message(message.from_user.id, 'Курс рос. рубля на сегодня ' + today_date_str + ' (за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub))));
-            bot.send_message(message.from_user.id, ' Курс рос. рубля на завтра ' + tomorrow_date_str + ' (за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub, tomorrow_date_str))));
+            bot.send_message(message.from_user.id, 'Курс доллара на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol))))
+            bot.send_message(message.from_user.id, ' Курс доллара на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(dol, tomorrow_date_str))))
+            bot.send_message(message.from_user.id, 'Курс евро на сегодня ' + today_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro))))
+            bot.send_message(message.from_user.id, ' Курс евро на завтра ' + tomorrow_date_str + ' - ' + str(get_rate_from_json(get_rate_response(euro, tomorrow_date_str))))
+            bot.send_message(message.from_user.id, 'Курс рос. рубля на сегодня ' + today_date_str + ' (за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub))))
+            bot.send_message(message.from_user.id, ' Курс рос. рубля на завтра ' + tomorrow_date_str + ' (за 100 рос. рублей) - ' + str(get_rate_from_json(get_rate_response(rub, tomorrow_date_str))))
         else:
             bot.send_message(message.from_user.id,"Моя твоя не понимать. Список вопросов, на которые я могу ответить ты найдёшь по запросу 'help'.")
     except Exception as e:
